cbfc915179e40e50a71d91df9417da4b2268ecd5
Panashe M. Fundira <fundirap@gmail.com>
2015-09-25 16:21:46 -0400

    remove comments

--- src/BinHeight.java
+++ src/BinHeight.java
@@ -1,10 +1,6 @@
-// java.util.* has been imported for this problem.
-// You don't need any other imports.
 
 public int findHeight(TreeNode root) { 
-    // Add your code below this line. Do not modify any other code.                   
     if (root == null)
         return 0;
     return 1 + Math.max(findHeight(root.left), findHeight(root.right));
-    // Add your code above this line. Do not modify any other code.
 }
